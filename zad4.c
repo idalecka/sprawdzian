44d5fb20123a2eb99941b50869b25b1fb3a04a22
Izabella Dalecka <idalecka@sigma.inf.ug.edu.pl>
2011-01-18 11:06:35 +0100

    zad4

---
+++ zad4.c
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+plik=`python -c "import random; print random.randrange(find -type f 
+-all);"`
+echo Czy usunac plik?(t/n):
+echo $plik 
+read pl
+
+while (($plik!=$pl));do
+
+if (($t));then
+echo "Usuwam plik"
+else
+echo "plik nie zostal usuniety"
+
+fi
+sleep 1
+
+done
+echo oki
+
